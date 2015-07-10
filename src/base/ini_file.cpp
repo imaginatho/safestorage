96b688f31545d3ca0639a248824656449ecd8777
team@imaginatho.com <team@imaginatho.com>
2015-07-10 07:23:05 +0200

    create header of class CIniFile

--- src/base/ini_file.cpp
+++ src/base/ini_file.cpp
@@ -6,51 +6,7 @@
 #include <string.h>
 #include <stdio.h>
 
-#include <string>
-#include <list>
-#include <map>
-
-using namespace std;
-
-#ifndef MAX_INI_FILE_SIZE
-#define MAX_INI_FILE_SIZE (512 * 1024)
-#endif 
-
-
-typedef map<string, string> CIniFileGroupValues;
-
-class CIniFileGroup
-{
-	public:
-		CIniFileGroupValues values;
-};
-
-typedef map<string, CIniFileGroup *> CIniFileGroups;
-typedef list<string> CIniFileGroupNames;
-
-class CIniFile
-{
-	protected:
-		char *content;
-		string currentGroup;
-		CIniFileGroups groups;
-		CIniFileGroup defaultGroup;
-		CIniFileGroupNames gnames;		// to get same order that ini file
-		
-		int loadFile ( const string &filename );
-		void parse ( void );
-		void addKeyValue ( const string &group, const string &key, const string &value );
-	public:
-		CIniFile ( const string &filename );
-		~CIniFile ( void );
-		const string &internalGetParam ( const string &group, const string &key, const string &defaultValue = "" );
-		const string &getStrParam ( const string &key, const string &defaultValue = "" );
-		void setGroup ( const string &group ) { currentGroup = group; };
-		uint32_t getUIntParam ( const string &key, uint32_t defaultValue = 0 );
-		int32_t getIntParam ( const string &key, int32_t defaultValue = 0  );
-		const string &getWildcardMatchGroup ( const string &name, const string &defaultvalue = "" );
-		static bool wildcardMatch ( const char *pattern, const char *str );
-};
+#include <ini_file.h>
 
 CIniFile::CIniFile ( const string &filename )
 {
